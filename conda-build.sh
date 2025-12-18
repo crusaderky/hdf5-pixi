86230c4e5e3b3bc849cbabd1064cccd5800d59ed
Guido Imperiale <crusaderky@gmail.com>
2025-12-18 17:25:00 +0000

    pixi build (#3)

--- conda-build.sh
+++ conda-build.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 set -ex
 
+CC="clang"
+export CMAKE_BUILD_PARALLEL_LEVEL="4"
+export CTEST_PARALLEL_LEVEL="4"
+export CMAKE_CONFIG_TYPE="Debug"
+export CMAKE_BUILD_TYPE="Debug"
+# export CMAKE_CONFIG_TYPE="Release"
+# export CMAKE_BUILD_TYPE="Release"
+export HDF5_CONFIG_ARGS="-DHDF5_ENABLE_DEBUG_APIS=ON -DHDF5_ENABLE_TRACE=ON -DHDF5_ENABLE_COVERAGE=ON"
+# export VERBOSE="1"
+
+pushd "$RECIPE_DIR"/..
+cmake -S hdf5 -B $BUILD_DIR $HDF5_CONFIG_ARGS
+cd $BUILD_DIR
+cmake --build .
+cpack -V
+popd
+
 # Find the tarball in the build directory
-TARBALL=$(ls "$RECIPE_DIR"/../build/HDF5-*.tar.gz 2>/dev/null | head -1)
+TARBALL=$(ls "$RECIPE_DIR"/../$BUILD_DIR/HDF5-*.tar.gz 2>/dev/null | head -1)
 
 if [ -z "$TARBALL" ]; then
-    echo "Error: No HDF5 tarball found in build/ directory"
-    echo "Please run 'pixi run pack' first to create the tarball"
+    echo "Error: No HDF5 tarball found in $BUILD_DIR directory"
     exit 1
 fi
 
